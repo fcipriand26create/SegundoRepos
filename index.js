458233e721bd3e41a11ad7d9c2db38e2c45d5a5e
fcipriand <franklincd26@gmail.com>
2025-01-18 19:47:37 -0500

    add new cod

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ import React from 'react'
 
 
 function Home() {
-    const ListItem = {}
-}
+    const ListItem = [1,2,3,4]
+
+    const addItem=()=>{
+        const variableA=1;
+        const variableB=3;
+        const variableC=2;
+
+        return variableA*3 +variableB*variableC
+    }
+} 
